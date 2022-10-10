7fd0f0b7415beea6040512d3907c245551459525
Sinforge <vlad.vlasov77@mail.ru>
2022-10-10 13:03:30 +0300

    add carusuel on main page and fix user comment img

---
+++ AnimeHouse/wwwroot/js/Carusuel.js
@@ -0,0 +1,31 @@
+﻿let slideIndex = 1;
+showSlide(slideIndex);
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+
+function thisSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i=0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
