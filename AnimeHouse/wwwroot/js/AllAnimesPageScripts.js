1f0b1080d68613b32e2c94266a582c6fb8cdf416
Sinforge <vlad.vlasov77@mail.ru>
2022-10-26 12:13:21 +0300

    change animation with anime containers

--- AnimeHouse/wwwroot/js/AllAnimesPageScripts.js
+++ AnimeHouse/wwwroot/js/AllAnimesPageScripts.js
@@ -13,4 +13,26 @@
             }
         });
     });
+    let containers = document.getElementsByClassName("anime-container");
+    
+    $('.anime-container').mouseover(function () {
+        let conts = document.getElementsByClassName("anime-container");
+        let currentElem = this;
+        $('.anime-container').each(function (index, item) {
+            if (item.getAttribute('id') === currentElem.getAttribute('id')) {
+                item.style.filter = "brightness(120%)";
+            } else {
+                item.style.filter = "blur(5px)";
+            }
+        });
+    });
+    $('.anime-container').mouseout(function () {
+        
+        $('.anime-container').each(function (index, item) {
+            item.style.filter = "none";
+        });
+    });
+    
+
+    
 });
